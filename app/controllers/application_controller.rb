241a0ea7c4ae303e2b2d24cf916478da1a1dd634
Olivia Kesler <oakesler@protonmail.ch>
2020-01-03 09:55:09 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
-
 require_relative '../../config/environment'
+
+require 'pry'
 
 class ApplicationController < Sinatra::Base
 
@@ -10,4 +11,32 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+  
+  get '/articles/new' do 
+    erb :new 
+  end
+  
+  post "/articles" do
+    @articles = Article.create(title: params[:title], content: params[:content])
+    erb :show
+  end
+  
+  get "/articles" do 
+    @articles = Article.all
+    erb :index
+  end
+  
+  get "/articles/:id" do
+    @article = Article.find(params[:id])
+    
+    erb :show
+  end
+  
+  get '/articles/:id/edit' do 
+    erb :edit
+  end
+  
+  delete '/articles/:id'
+    
+    
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,7 @@
 #Placeholder for a model
 
 class Article < ActiveRecord::Base
+  
+  attr_accessor :title, :content
+  
 end
